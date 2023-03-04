e17972e093ea4f6ba6682c1a4b061ada42985e2e
DiegoCedresE <diego.cedres101@alu.ulpgc.es>
2023-03-04 12:13:57 +0000

    version

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+$(function() {
+    init();
+});
+
+function init() {
+    initHeader();
+    initFooter();
+}
+
+function initHeader() {
+    $("header").load("./templates/header.html");
+}
+
+function initFooter() {
+    $("footer").load("../templates/footer.html")
+}
